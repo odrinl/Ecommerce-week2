8a452caa282f657b11fd358f00d4ffc690ba41b2
Audrey <keozon@gmail.com>
2023-09-27 01:25:09 +0200

    changed github link

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <footer className='bg-body-secondary text-light py-5'>
         <div className='container text-center'>
           <a
-            href='https://github.com/odrinl/React-ecommerce'
+            href='https://github.com/odrinl/Ecommerce-week2'
             className='link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover'
           >
             &copy; Audrey 2023
